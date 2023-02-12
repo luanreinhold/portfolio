f6488c972db0b2b29e8e925d5cc1dc121b144b7b
Luan Reinhold <anjobranco67@gmail.com>
2023-02-11 23:46:41 -0300

    finalização

--- site/src/components/modal/index.js
+++ site/src/components/modal/index.js
@@ -9,13 +9,14 @@ function ModalProjeto({ exibir, fechar }) {
                    <h1>Store Games</h1>
                     <button onClick={fechar}>Fechar</button>
                 </div>
-                <p>Este foi meu projeto de conclusão de curso (tcc). Onde eu e meu grupo criamos um e-commerce com a visão administrativa e a do usuario. A ideia era de trazer o mais próximo possível a experiência de um e-commerce real</p>
-                <p>Participei de todas as etapas do projeto, como: a criação do layout com design low-fidelity e high-fidelity no figma, o planejamento e criação de todo o ddl e dml do banco de dados,
+                <p>Este foi meu projeto de conclusão de curso (tcc). Onde eu e meu grupo produzimos um e-commerce com a visão administrativa e a do usuário. A ideia era de trazer o mais próximo possível a experiência de um e-commerce real</p>
+                <p>Participei de todas as etapas do projeto, como: a criação do layout low-fidelity e high-fidelity no figma, planejamento e criação de todo o ddl e dml do banco de dados,
                     integração do mysql ao front end com api em nodejs, implementação das páginas com técnicas de re-uso, componentes reutilizaveis e afins.
                 </p>
                 <p>Tecnologias usadas: React, Javascript, Nodejs, Html5, Css, Sass, React, Figma</p>
-                <p>Para verificar todas as funcionalidades acesse o repositorio do projeto através da imagem abaixo: </p>
-               <img src="./images/github-original-wordmark.svg" alt="github" />
+                <p>Para verificar todas as funcionalidades e figma acesse o repositorio do projeto através da imagem abaixo: </p>
+              <a href="https://github.com/luanreinhold/Store-Games"> <img src="./images/github-original-wordmark.svg" alt="github" /> </a>
+
             </section>
         </main>
 
--- site/src/pages/home/mainpage.js
+++ site/src/pages/home/mainpage.js
@@ -11,8 +11,15 @@ function Home() {
 
   const [selecionarTexto, setSelecionarTexto] = useState('default');
 
+  const [statusArrow1, setStatusArrow1] = useState("arrow-invisible")
+  const [statusArrow2, setStatusArrow2] = useState("arrow-invisible")
+  const [statusArrow3, setStatusArrow3] = useState("arrow-invisible")
+  const [statusArrow4, setStatusArrow4] = useState("arrow-invisible")
+
   const [exibirModal, setExibirModal] = useState(false)
   const [exibirModalSql, setExibirModalSql] = useState(false)
+
+
 
   function modalAbrir() {
     setExibirModal(true)
@@ -30,29 +37,58 @@ function Home() {
     setExibirModalSql(false)
   }
 
-  function alterarTela (status) {
+  function alterarTela(status) {
 
     setSelecionarTexto(status)
   }
 
   useEffect(() => {
-    AOS.init({ duration: 2500 })
-}, [])
+    AOS.init({ duration: 2000 })
+
+  }, [])
+
+  function selecaoMenu() {
+
+    if (selecionarTexto !== null || undefined) {
+      setStatusArrow1('arrow-invisible')
+      setStatusArrow2('arrow-invisible')
+      setStatusArrow3('arrow-invisible')
+      setStatusArrow4('arrow-invisible')
+
+      if (selecionarTexto === 'default')
+        setStatusArrow1('arrow-visible')
+
+      if (selecionarTexto === 'tecnologia')
+        setStatusArrow2('arrow-visible')
+
+      if (selecionarTexto === 'projeto')
+        setStatusArrow3('arrow-visible')
+
+
+      if (selecionarTexto === 'contato')
+        setStatusArrow4('arrow-visible')
+    }
+
+  }
+
+  useEffect(() => {
+    selecaoMenu()
+  }, [selecionarTexto])
 
 
   return (
     <main className='main-home'>
       <ModalProjeto exibir={exibirModal} fechar={modalFechar} />
-      <ModalSql exibir={exibirModalSql} fechar={modalFecharSql}/>
+      <ModalSql exibir={exibirModalSql} fechar={modalFecharSql} />
 
       <section className='container-home flexbox-row '>
         {selecionarTexto === 'default' &&
-          <article data-aos="fade-up"  className='container-about-me naoSelecionavel fontd-quicksan'  >
-            <span>about me</span>
-            <p>
-              <h1>Meu nome é  Luan Reinhold, </h1>Sou formado em: Técnico em informática onde aprendi
-              a aplicar soluções através de páginas web. Busco sempre evoluir como desenvolvedor fullstack através de estudos e projetos. Atualmente estou no 1 período em Análise e desenvolvimento de sistemas,
-              onde procuro oportunidade como trainee/estagiario.
+          <article data-aos="fade" className='container-about-me naoSelecionavel fontd-quicksan'  >
+            <div data-aos="slide-left">about me</div>
+            <h1 data-aos="slide-right">Meu nome é  Luan Reinhold, </h1>
+            <p data-aos="zoom-in-up">Possuo formação técnica em informática onde aprendi
+              a desenvolver soluções através de tecnologias e ferramentas. <p>Busco sempre evoluir como desenvolvedor fullstack através de estudos e projetos. Atualmente estou cursando Análise e desenvolvimento de sistemas,
+                onde procuro oportunidade como estágiario/trainee para adquirir cada vez mais conhecimentos.</p>
             </p>
           </article>
         }
@@ -60,35 +96,38 @@ function Home() {
         {selecionarTexto === 'tecnologia' &&
           <section className='container-tecnologias fontd-quicksan '>
             <div>
-              <p data-aos="fade-down" style={{ 'color': 'white' }}>Sãos estas tecnologias e ferramentas que uso ao desenvolver aplicações</p>
+              <p data-aos="fade-down" style={{ 'color': 'white' }}>Tecnologias que possuo conhecimento:</p>
             </div>
-            <div data-aos="fade-up"  className='flexboxrow'>
+            <div data-aos="slide-right" className='flexboxrow tecnologia-row1'>
               <CardIcon imagem='./images/javascript.svg' nome='Javascript' />
               <CardIcon imagem='./images/nodejs-plain-wordmark.svg' nome='Node' />
-              <CardIcon imagem='./images/react-original.svg' nome='React' />
               <CardIcon imagem='./images/mysql-original.svg' nome='Mysql' />
+              <CardIcon imagem='./images/mongodb-original.svg' nome='MongoDB' />
             </div>
-            <div data-aos="fade-up" className='flexboxrow'>
+            <div data-aos="slide-up" className='flexboxrow tecnologia-row2'>
               <CardIcon imagem='./images/html.svg' nome='Html' />
               <CardIcon imagem='./images/css3-plain.svg' nome='Css' />
               <CardIcon imagem='./images/sass-original.svg' nome='Sass' />
-              <CardIcon imagem='./images/sass-original.svg' nome='Sass' />
+              <CardIcon imagem='./images/react-original.svg' nome='React' />
             </div>
           </section>
         }
 
         {selecionarTexto === 'projeto' &&
-          <section data-aos="fade" className="container-projetos">
-            <div onClick={modalAbrir} className="box-projeto">
-              <img src="./images/storegames-icon.svg" alt="icon loja" />
-              <div className="box-projeto-footer"><b>Store Games</b></div>
-            </div>
+          <div>
+            <p data-aos="slide-right" style={{ 'color': 'white', 'margin': '1rem' }} className="fontd-quicksan">Novos projetos em andamento...</p>
+            <section data-aos="fade-up" className="container-projetos">
+              <div onClick={modalAbrir} className="box-projeto">
+                <img src="./images/storegames-icon.svg" alt="icon loja" />
+                <div className="box-projeto-footer"><b>Store Games</b></div>
+              </div>
+              <div onClick={modalAbrirSql} className="box-projeto">
+                <img id='mysql-icon' src="./images/mysql-original.svg" alt="" />
+                <div className="box-projeto-footer"><b>Modelagens Mysql</b> </div>
+              </div>
+            </section>
 
-            <div onClick={modalAbrirSql} className="box-projeto">
-              <img id='mysql-icon' src="./images/mysql-original.svg" alt="" />
-              <div className="box-projeto-footer"><b>Modelagens Mysql</b> </div>
-            </div>
-          </section>
+          </div>
         }
 
         {selecionarTexto === 'contato' &&
@@ -99,7 +138,7 @@ function Home() {
               <label>luanreinhold</label>
             </div>
             <div data-aos="fade-down" className="card-linkedin">
-             <a href="https://www.linkedin.com/in/luanreinhold"><img src="./images/linkedin-plain-wordmark.svg" alt="linkedin icon" /></a> 
+              <a href="https://www.linkedin.com/in/luanreinhold"><img src="./images/linkedin-plain-wordmark.svg" alt="linkedin icon" /></a>
               <label>Luan Reinhold</label>
             </div>
             <div data-aos="fade-up" className="card-email">
@@ -110,11 +149,11 @@ function Home() {
 
         }
         <aside className='side-menu flexbox-column fontd-quicksan'>
-          <ul style={{ "list-style": "none" }}>
-            <li onClick={() => alterarTela('default')}>Sobre mim</li>
-            <li onClick={() => alterarTela('tecnologia')}>Tecnologias que uso</li>
-            <li onClick={() => alterarTela('projeto')}>Projetos</li>
-            <li onClick={() => alterarTela('contato')}>Contatos</li>
+          <ul className='nav-list' style={{ "list-style": "none" }}>
+            <div className="row-arrow"><img id={statusArrow1} src="./images/arrow-icon-arrows-sign-black-arrows-free-png.webp" alt="" /><li onClick={() => alterarTela('default')}>Sobre mim</li></div>
+            <div className="row-arrow"><img id={statusArrow2} src="./images/arrow-icon-arrows-sign-black-arrows-free-png.webp" alt="" /><li onClick={() => alterarTela('tecnologia')}>Tecnologias</li></div>
+            <div className="row-arrow"><img id={statusArrow3} src="./images/arrow-icon-arrows-sign-black-arrows-free-png.webp" alt="" /><li onClick={() => alterarTela('projeto')}>Projetos</li></div>
+            <div className="row-arrow"><img id={statusArrow4} src="./images/arrow-icon-arrows-sign-black-arrows-free-png.webp" alt="" /><li onClick={() => alterarTela('contato')}>Contatos</li></div>
           </ul>
         </aside>
 
